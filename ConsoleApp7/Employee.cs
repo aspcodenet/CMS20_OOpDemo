c3377f3fea783679d09743141dd336c6dc09bfd8
Stefan Holmberg <stefan.holmberg@systementor.se>
2020-09-15 10:18:05 +0200

    Add project files.

---
+++ ConsoleApp7/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int HourlySalary { get; set; }
+
+        public string StreetAddress { get; private set; }
+        public int PostalCode { get; private set; }
+        public string City { get; private set; }
+
+        public Employee(DateTime birthDate, int hourlySalary)
+        {
+            BirthDate = birthDate;
+            HourlySalary = hourlySalary;
+        }
+
+        public void ChangeAddress(string streetAddress, int postalCode, string city)
+        {
+            StreetAddress = streetAddress;
+            PostalCode = postalCode;
+            City = city;
+            //SendMail
+        }
+
+        public decimal CalculateSalary(int hours)
+        {
+            if (BirthDate.Year < 1973)
+                hours = hours + 10;
+            return hours * HourlySalary;
+        }
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee employee = new Employee(new DateTime(1972,8,3),123  );
+            employee.Name = "Stefan Holmberg";
+            decimal d = employee.CalculateSalary(20);
+
+
+            Employee employee2 = new Employee(new DateTime(2002, 3, 30),500);
+            employee2.Name = "Josefine Holmberg";
+            d = employee2.CalculateSalary(20);
+
+            Console.WriteLine("Hello World!");
+
+
+            //Input
+            var newStreet = "Hoppgatan 12";
+            var newCity = "Nystad";
+            int newPostal = 1234;
+            employee2.ChangeAddress(newStreet, newPostal,newCity);
+            Console.WriteLine(employee2.StreetAddress);
+
+        }
+    }
+}
